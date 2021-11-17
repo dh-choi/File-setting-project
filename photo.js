a7abd7eb0c76b9c1821f3f14c79275dca307c540
Dahye Choi <dh.choi6900@gmail.com>
2021-11-17 11:05:50 +0900

    초기설정

---
+++ photo.js
@@ -0,0 +1,10 @@
+/*
+    파일 정리 프로젝트
+
+    파일의 확장자가 .mp4, .mov일 경우 viedo 폴더 안에 넣어줌
+    .png, .aae일 경우 captured
+    IMG_1234 와 IMG_E1234가 있을 경우 IMG_1234만 duplicated에 넣어줌
+
+    실행 방법
+    node  photo test 할 경우 자동으로 사진 정리
+*/
