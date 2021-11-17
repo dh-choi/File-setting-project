1dbe22f2e160ccbb5339d2ee7957c11d8267d783
Dahye Choi <dh.choi6900@gmail.com>
2021-11-17 19:40:36 +0900

    feat

--- photo.js
+++ photo.js
@@ -8,3 +8,25 @@
     실행 방법
     node  photo test 할 경우 자동으로 사진 정리
 */
+const { dir } = require('console');
+const fs = require('fs');
+const path = require('path');
+
+const video_ext = ['.mp4', '.mov'];
+const captured_ext = ['.png', '.aae'];
+let dirname;
+
+function photo_setting(dirname) {
+  this.dirname = path.join(__dirname + path.sep + dirname);
+  console.log(this.dirname);
+  fs.promises
+    .readdir(this.dirname)
+    .then((file_name) => {
+      sort_file(file_name);
+    })
+    .catch(console.error);
+}
+
+const sort_file = (file_name) => {};
+
+photo_setting('test');
